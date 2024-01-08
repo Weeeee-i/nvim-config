4f16adce8e01e6bc9b62542463d6dfeaf3ae17aa
Weeeee-i <weeeeei@duck.com>
2024-01-08 10:26:27 +0800

    config helix editor

--- home/applications/editor.nix
+++ home/applications/editor.nix
@@ -16,4 +16,16 @@
 
   home.packages = with pkgs; [ helix ];
 
+  home.file.helix-config = {
+    enable = true;
+    source = ./conf-helix/config.toml;
+    target = ".config/helix/config.toml";
+  };
+
+  home.file.helix-languages = {
+    enable = true;
+    source = ./conf-helix/languages.toml;
+    target = ".config/helix/languages.toml";
+  };
+
 }
