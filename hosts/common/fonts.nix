e0f2b03569a45127f0442ea0b452195bcf9c7a55
Weeeee-i <weeeeei@duck.com>
2024-01-09 08:26:40 +0800

    add nerdfont

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -7,13 +7,13 @@
       noto-fonts
       noto-fonts-cjk
       jetbrains-mono
-      fira-code-nerdfont
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
     ];
 
     fontconfig.defaultFonts = {
       serif = [ "Noto Serif CJK SC" ];
       sansSerif = [ "Noto Sans CJK SC" ];
-      monospace = [ "JetBrains Mono" ];
+      monospace = [ "JetBrainsMono NFT" ];
     };
 
   };
